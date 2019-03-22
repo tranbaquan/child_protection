d0b17a32005fbad32ab2955696c87bcc0ae1c326
tranbaquan <15130148@st.hcmuaf.edu.vn>
2019-03-22 23:05:55 +0700

    init by tranbaquan 22/03/2019

---
+++ src/main/java/edu/hcmuaf/helloworld/childprotection/ChildProtectionApplication.java
@@ -0,0 +1,13 @@
+package edu.hcmuaf.helloworld.childprotection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildProtectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildProtectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hcmuaf/helloworld/childprotection/ChildProtectionApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.hcmuaf.helloworld.childprotection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChildProtectionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
